409d561fba5ab6a735854da50e2f7eb5b925f997
Lythenghuy Keo <102404121+LythenghuyKeo@users.noreply.github.com>
2023-09-11 13:22:22 +0700

    Update connectDB.go

--- backend/initializers/connectDB.go
+++ backend/initializers/connectDB.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "host=localhost user=postgres password=keochanny$1 dbname=golang-api port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	//dsn := "host=localhost user=postgres password=your_password dbname=golang-api port=5432 sslmode=disable TimeZone=Asia/Bangkok"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to db")
